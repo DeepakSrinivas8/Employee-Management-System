cd33f670e958bdcfc74cdda3c0bcf15dcf52c1d2
DeepakSrinivas8 <106883658+DeepakSrinivas8@users.noreply.github.com>
2025-03-25 22:40:39 +0530

    Add files via upload

---
+++ employee-management-system/src/test/java/com/imaginnovate/employee_management_system/ImaginnovateEmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imaginnovate.employee_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImaginnovateEmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
